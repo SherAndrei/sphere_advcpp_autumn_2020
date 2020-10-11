99878a5736398f29862998a79580d75c425a14b5
sherandrei <welldryus@gmail.com>
2020-10-11 18:39:15 +0300

    changes

---
+++ 1.Process/descriptor.h
@@ -0,0 +1,23 @@
+#ifndef DESCRIPTOR_H
+#define DESCRIPTOR_H
+#include <unistd.h>
+
+class Descriptor
+{
+public:
+	bool isValid() const;
+
+public:
+	Descriptor() = default;
+	explicit Descriptor(pid_t pid);
+	~Descriptor();
+	
+	Descriptor(const Descriptor& other);
+public:
+	pid_t  operator()() const;
+	pid_t& operator()(pid_t pid);
+private:
+	pid_t _pid;
+};
+
+#endif //DESCRIPTOR_H
